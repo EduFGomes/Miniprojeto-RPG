c20750d7fd6f76ef691bb926054cd2a1db745ab4
eduardapicolo <eduardapicoloo@outlook.com>
2025-10-27 08:46:18 -0300

    arrumando classe Gigante

--- Gigante.java
+++ Gigante.java
@@ -4,42 +4,6 @@ public class Gigante extends Inimigo {
     private static int ATAQUE_FIXO = 45;
     private static int DEFESA_FIXA = 60;
 
-    private int furia;
-
-    public Gigante(String nome, byte nivel, Inventario inventario, int furiaInicial) throws Exception {
-        super(nome, VIDA_FIXA, ATAQUE_FIXO, DEFESA_FIXA, nivel, inventario, "Gigante");
-        this.furia = furiaInicial;
-    }
-
-    public void pisaoEsmagador() {
-        if (this.furia >= 15) {
-            System.out.println(this.nome + " usa um Pisão Esmagador!");
-            this.furia -= 15;
-        } else {
-            System.out.println(this.nome + " está exausto.");
-        }
-    }
-    
-    public int getFuria() {
-        return furia;
-    }
-
-    public void setFuria(int furia) {
-        this.furia = Math.max(0, furia);
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + "\n" + "Fúria: " + this.furia;
-    }
-}
-
-public class Gigante extends Inimigo {
-
-    private static int VIDA_FIXA = 800;
-    private static int ATAQUE_FIXO = 45;
-    private static int DEFESA_FIXA = 60;
-
     public Gigante(String nome, byte nivel, Inventario inventario) throws Exception {
         super(nome, VIDA_FIXA, ATAQUE_FIXO, DEFESA_FIXA, nivel, inventario, "Gigante");
     }
